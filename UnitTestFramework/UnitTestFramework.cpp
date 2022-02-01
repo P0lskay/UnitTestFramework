3077f40507c6fab4d2b81dc540a117a0ede72bb9
P0lskay <gg.lol.2264@gmail.com>
2022-02-01 16:28:14 +0300

    Добавьте файлы проекта.

---
+++ UnitTestFramework/UnitTestFramework.cpp
@@ -0,0 +1,23 @@
+﻿#include <exception>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <typename T, typename U>
+void AssertEqual(const T& t, const U& u)
+{
+    if (t != u)
+    {
+        ostringstream os;
+        os << "Assertion failed: " << t << " != " << u;
+        throw runtime_error(os.str());
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
