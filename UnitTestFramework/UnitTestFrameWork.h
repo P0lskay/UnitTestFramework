8b0a8018c57e2afae24cb3bdd65fc74c4191821b
P0lskay <gg.lol.2264@gmail.com>
2022-03-03 20:28:57 +0300

    Добавлены макросы

--- UnitTestFramework/UnitTestFrameWork.h
+++ UnitTestFramework/UnitTestFrameWork.h
@@ -1,54 +1,116 @@
 #pragma once
 
-#include <iostream>
-#include <string>
 #include <sstream>
+#include <stdexcept>
+#include <iostream>
 #include <map>
 #include <set>
+#include <string>
 #include <vector>
 
-void Assert(bool b, const string& hint);
+using namespace std;
 
-class TestRunner {
-public:
-    ~TestRunner();
-
-    template <typename TestFunc>
-    void RunTest(TestFunc func, const string& name_test)
-    {
-        try
-        {
-            func();
-            cerr << name_test << " OK!" << endl;
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
         }
-        catch (runtime_error& e)
-        {
-            fail_counter++;
-            cerr << name_test << " is fail: " << e.what() << endl;
-        }
+        first = false;
+        os << x;
     }
-private:
-    int fail_counter = 0;
-};
-
-template <typename Key, typename Value>
-ostream& operator << (ostream& os, const map<Key, Value>& mp)
-{
-    for (auto& item : mp)
-    {
-        os << item.first << ": {" << item.second << "}";
-    }
-    return os;
+    return os << "}";
 }
 
-template <typename T, typename U>
-void AssertEqual(const T& t, const U& u, const string& hint)
-{
-    if (t != u)
-    {
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << x;
+    }
+    return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+    if (!(t == u)) {
         ostringstream os;
-        os << "Assertion failed: " << t << " != " << u
-            << "Hint: " << hint;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+            os << " hint: " << hint;
+        }
         throw runtime_error(os.str());
     }
 }
+
+inline void Assert(bool b, const string& hint) {
+    AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const string& test_name) {
+        try {
+            func();
+            cerr << test_name << " OK" << endl;
+        }
+        catch (exception& e) {
+            ++fail_count;
+            cerr << test_name << " fail: " << e.what() << endl;
+        }
+        catch (...) {
+            ++fail_count;
+            cerr << "Unknown exception caught" << endl;
+        }
+    }
+
+    ~TestRunner() {
+        if (fail_count > 0) {
+            cerr << fail_count << " unit tests failed. Terminate" << endl;
+            exit(1);
+        }
+    }
+
+private:
+    int fail_count = 0;
+};
+
+#define ASSERT_EQUAL(x, y) {            \
+  ostringstream os;                     \
+  os << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;     \
+  AssertEqual(x, y, os.str());          \
+}
+
+#define ASSERT(x) {                     \
+  ostringstream os;                     \
+  os << #x << " is false, "             \
+    << __FILE__ << ":" << __LINE__;     \
+  Assert(x, os.str());                  \
+}
+
+#define RUN_TEST(tr, func) \
+  tr.RunTest(func, #func)
+
